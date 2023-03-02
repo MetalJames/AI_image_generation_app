4dc81a90ff45f40e306b9d5232df9dfbb6b22a72
Volodymyr <volodyaruzhak@gmail.com>
2023-03-02 11:13:57 -0500

    init commit

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+
+import logo from './assets/logo.svg';
+import { Home, CreatePost } from './pages';
+
+const App = () => (
+ <>
+ </>
+);
+
+export default App;

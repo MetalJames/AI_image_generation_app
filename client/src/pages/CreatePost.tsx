047560b55f693f8d4f22338b6c44d672d9a35226
Volodymyr <volodyaruzhak@gmail.com>
2023-03-03 14:36:02 -0500

    generateImage function implemented working amazing!

--- client/src/pages/CreatePost.tsx
+++ client/src/pages/CreatePost.tsx
@@ -16,9 +16,31 @@ const CreatePost = () => {
     const [generatingImg, setGeneratingImg] = useState(false);
     const [loading, setLoading] = useState(false);
 
-    const generateImage = () => {
+    const generateImage = async () => {
+        if (form.prompt) {
+            try {
+                setGeneratingImg(true);
+                const response = await fetch('http://localhost:8080/api/v1/walle', {
+                // const response = await fetch('https://dalle-arbb.onrender.com/api/v1/walle', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ prompt: form.prompt }),
+                });
 
-    }
+                const data = await response.json();
+                setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
+
+            } catch (err) {
+                alert(err);
+            } finally {
+                setGeneratingImg(false);
+            }
+        } else {
+        alert('Please enter a prompt');
+        }
+    };
 
     const handleSubmit = async (e: any) => {
         e.preventDefault();

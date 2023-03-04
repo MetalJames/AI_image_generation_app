ad8a7fc7fc9e179706cbdb998471b646d330f491
Volodymyr <volodyaruzhak@gmail.com>
2023-03-03 21:14:56 -0500

    I hope its finalle commit was a server long response but everything is working great

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -30,11 +30,13 @@ const Home = () => {
     const [searchedResults, setSearchedResults] = useState<null | any>(null!);
     const [searchTimeout, setSearchTimeout] = useState<null | any>(null!);
 
-    const fetchPosts = async () => {
+    useEffect(() => {
+        const fetchPosts = async () => {
             setLoading(true);
 
             try {
                 const response = await fetch("https://wall-e-131k.onrender.com/api/v1/post",{
+                // const response = await fetch("http://localhost:8080/api/v1/post",{
                     method: "GET",
                     headers: {
                     "Content-Type": "application/json",
@@ -52,7 +54,6 @@ const Home = () => {
             }
         };
 
-    useEffect(() => {
         fetchPosts();
     }, []);
 
